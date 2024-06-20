3bb0d4148fb48c003effa9cabd7a7b5078bcd8f2
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2024-06-20 22:51:29 +0530

    hyu

---
+++ Day 4/Task set 2/gallery.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('footer nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
