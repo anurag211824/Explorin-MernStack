ec30076d25c5fc78846498a7a9d49843b8a1128a
Anurag Kumar <138526681+anurag211824@users.noreply.github.com>
2024-06-22 16:38:00 +0530

    learnt eventListener and document.get

---
+++ Day 8/script.js
@@ -0,0 +1,39 @@
+console.log("hello world!");
+
+let pTags = document.getElementsByTagName("p");
+//console.log(pTag);
+
+
+let currentDate = new Date();
+
+
+let datestring = currentDate.getDate() + "/" +
+    (currentDate.getMonth() + 1) + "/" +
+    currentDate.getFullYear();
+
+
+// pTag.innerHTML = `
+// <a href="#">Today Date</a>: ${datestring}
+// `;
+console.log(datestring);
+
+// Get the paragraph elements by class name
+let pTagByClass = document.getElementsByClassName("demo-p");
+let numPtags = pTagByClass.length;
+
+// Iterate over the collection and set the innerHTML for each
+for (let pTag of pTags) {
+    console.log(pTag);
+    pTag.innerHTML = `
+    <a href="#">Today Date</a>: ${datestring}
+    `;
+}
+
+// Add event listeners for mouse hover and mouse out
+let heading = document.getElementById("heading");
+heading.addEventListener("mouseover", function() {
+    this.innerHTML = "Hovered";
+});
+heading.addEventListener("mouseout", function() {
+    this.innerHTML = "Unhovered";
+});
